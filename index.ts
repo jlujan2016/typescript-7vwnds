5363d9287914d149c473e9c003afb7f445755c34
jlujan2016 <lujan_exogenesis@hotmail.com>
2021-10-09 20:32:47 -0500

    For Of/ In

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 let fullName = 'jose lujan';
 console.log('el nombre es' + fullName + 'aprobado');
 console.log(`El nombre es  ${fullName} aprobado`);
-CharacterDat
+let numbers = [1, 2, 3, 4, 5];
+for (let number of numbers) {
+  console.log(number);
+}
+
+for (let index in numbers) {
+  console.log(index);
+}
